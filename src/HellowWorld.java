6c979227bacca0c6b3a4326071a9f25d23094ac4
kmtarek <kmtarekfca@gmail.com>
2021-07-09 23:24:42 +0600

    print 2nd time

--- src/HellowWorld.java
+++ src/HellowWorld.java
@@ -1,6 +1,12 @@
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins");
+
+        System.out.println("Hello Jenkins 2nd time !");
+
+
+
+
     }
 
 
